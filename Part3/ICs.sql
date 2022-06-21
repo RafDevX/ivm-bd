f025b652c22a8071cfbc07d583a67f303aadf4ee
Rafael Oliveira <56204853+RafDevX@users.noreply.github.com>
2022-06-21 20:07:37 +0100

    Integrity Constraints (#21)
    * remove names from foreign key constraints
    
    * implement RI-1
    
    category cannot be contained in itself
    
    * implement RI-4
    
    evento_reposicao.unidades cannot be greater than planograma.unidades
    
    * implement RI-5
    
    only allow resupplying products onto shelves with one of the product's
    categories
    
    * move schema creation into schema.sql
    
    split populate.sql into two, moving schema creation part to schema.sql
    and keeping only the data population instructions.
    
    this allows running ICs.sql in between them
    
    * fix typos
    
    * implement RI-RE4
    
    don't allow duplicate combinations of (super_categoria, categoria)
    
    fix wrong primary key
    
    * formatting
    
    * fix bad data in populate.sql
    
    Co-authored-by: Luís Fonseca <luis@lhf.pt>

---
+++ Part3/ICs.sql
@@ -0,0 +1,48 @@
+/* O número de unidades repostas  num evento de reposição não pode exceder o número de unidades especificado no planograma */
+CREATE OR REPLACE FUNCTION chk_oversupply() RETURNS TRIGGER AS
+$$
+DECLARE max_units INTEGER;
+BEGIN
+	SELECT unidades INTO max_units
+		FROM planograma
+		WHERE ean = NEW.ean
+		AND nro = NEW.nro
+		AND num_serie = NEW.num_serie
+		AND fabricante = NEW.fabricante;
+	IF NEW.unidades > max_units THEN
+		RAISE EXCEPTION 'Event units exceeds max allowed by planogram';
+	END IF;
+
+	RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS trigger_oversupply ON evento_reposicao;
+CREATE TRIGGER trigger_oversupply
+	BEFORE UPDATE OR INSERT ON evento_reposicao
+	FOR EACH ROW EXECUTE PROCEDURE chk_oversupply();
+
+/* RI-5: Um produto só pode ser reposto numa prateleira que apresente (pelo menos) uma das categorias desse produto */
+CREATE OR REPLACE FUNCTION chk_presentable() RETURNS TRIGGER AS
+$$
+BEGIN
+	IF NOT EXISTS (
+		SELECT nome
+		FROM prateleira
+		NATURAL JOIN tem_categoria
+		WHERE ean = NEW.ean
+		AND nro = NEW.nro
+		AND num_serie = NEW.num_serie
+		AND fabricante = NEW.fabricante
+	) THEN
+		RAISE EXCEPTION 'Product can only be resupplied onto shelves that allow one of its categories';
+	END IF;
+
+	RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS trigger_presentable ON evento_reposicao;
+CREATE TRIGGER trigger_presentable
+	BEFORE UPDATE OR INSERT ON evento_reposicao
+	FOR EACH ROW EXECUTE PROCEDURE chk_presentable();
