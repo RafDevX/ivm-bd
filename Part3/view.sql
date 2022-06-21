fa5feb166a92a5ad6ecea38acc942fe91a1e5a55
Rafael Oliveira <rafdev.x@gmail.com>
2022-06-21 21:51:11 +0100

    formatting

--- Part3/view.sql
+++ Part3/view.sql
@@ -1,20 +1,20 @@
 DROP VIEW IF EXISTS Vendas;
 
 CREATE VIEW Vendas(ean, cat, ano, trimestre, mes, dia_mes, dia_semana, distrito, concelho, unidades) AS
-  SELECT
-    er.ean,
-    er.nome,
-    extract(YEAR FROM er.instante),
-    extract(QUARTER FROM er.instante),
-    extract(MONTH FROM er.instante),
-    extract(DAY FROM er.instante),
-    extract(DOW FROM er.instante),
-    pr.distrito,
-    pr.concelho,
-    er.unidades
-  FROM (
-      evento_reposicao
-      NATURAL JOIN instalada_em
-      NATURAL JOIN tem_categoria
-    ) AS er
-    INNER JOIN ponto_de_retalho AS pr ON (er.local = pr.nome);
+	SELECT
+		er.ean,
+		er.nome,
+		EXTRACT(YEAR FROM er.instante),
+		EXTRACT(QUARTER FROM er.instante),
+		EXTRACT(MONTH FROM er.instante),
+		EXTRACT(DAY FROM er.instante),
+		EXTRACT(DOW FROM er.instante),
+		pr.distrito,
+		pr.concelho,
+		er.unidades
+	FROM (
+		evento_reposicao
+		NATURAL JOIN instalada_em
+		NATURAL JOIN tem_categoria
+	) AS er
+	INNER JOIN ponto_de_retalho AS pr ON (er.local = pr.nome);
