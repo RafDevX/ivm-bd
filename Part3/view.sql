19c47af326d24bb0a542a04260defa4ba7859f84
Luís Fonseca <luis.h.fonseca@tecnico.ulisboa.pt>
2022-06-22 10:25:39 +0100

    Views (#22)
    Co-authored-by: Rafael Oliveira <rafdev.x@gmail.com>
    Co-authored-by: Rafael Oliveira <56204853+RafDevX@users.noreply.github.com>

---
+++ Part3/view.sql
@@ -0,0 +1,20 @@
+DROP VIEW IF EXISTS Vendas;
+
+CREATE VIEW Vendas(ean, cat, ano, trimestre, mes, dia_mes, dia_semana, distrito, concelho, unidades) AS
+	SELECT
+		er.ean,
+		er.nome,
+		EXTRACT(YEAR FROM er.instante),
+		EXTRACT(QUARTER FROM er.instante),
+		EXTRACT(MONTH FROM er.instante),
+		EXTRACT(DAY FROM er.instante),
+		EXTRACT(DOW FROM er.instante),
+		pr.distrito,
+		pr.concelho,
+		er.unidades
+	FROM (
+		evento_reposicao
+		NATURAL JOIN instalada_em
+		NATURAL JOIN tem_categoria
+	) AS er
+	INNER JOIN ponto_de_retalho AS pr ON er.local = pr.nome;
