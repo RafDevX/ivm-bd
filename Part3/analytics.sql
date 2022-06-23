c239f26801545e6726e65288951925b5d75c0dab
Rafael Oliveira <rafdev.x@gmail.com>
2022-06-23 14:17:33 +0100

    implement olap queries

---
+++ Part3/analytics.sql
@@ -0,0 +1,14 @@
+/* 1. num dado período (i.e. entre duas datas), por dia da semana, por concelho e no total */
+
+SELECT dia_semana, concelho, SUM(unidades)
+	FROM Vendas
+	WHERE make_date(ano :: int, mes :: int, dia_mes :: int)
+		BETWEEN make_date(2019, 01, 01) AND make_date(2023, 12, 31)
+	GROUP BY GROUPING SETS (dia_semana, concelho, ());
+
+/* 2. num dado distrito (i.e. "Lisboa"), por concelho, categoria, dia da semana e no total */
+
+SELECT conselho, cat, dia_semana, SUM(unidades)
+	FROM Vendas
+	WHERE distrito = 'Lisboa'
+	GROUP BY CUBE(concelho, cat, dia_semana);
